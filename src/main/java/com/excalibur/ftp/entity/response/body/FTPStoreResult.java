a08b4f6635c72f908c1be413a0fa98b7bd570620
A.Syromyatnikov <a.syromyatnikov@fleetcor.ru>
2020-02-07 01:17:10 +0300

    store file security update

--- src/main/java/com/excalibur/ftp/entity/response/body/FTPStoreResult.java
+++ src/main/java/com/excalibur/ftp/entity/response/body/FTPStoreResult.java
@@ -4,25 +4,25 @@ import java.util.List;
 
 public class FTPStoreResult extends Result {
 
-    private String userId;
+    private String key;
     private String fileName;
 
-    public FTPStoreResult(boolean success, String fileName, String userId, List<String> errors) {
+    public FTPStoreResult(boolean success, String fileName, String key, List<String> errors) {
         super(success, errors);
         this.fileName = fileName;
-        this.userId = userId;
+        this.key = key;
     }
 
-    public String getUserId() {
-        return userId;
+    public String getKey() {
+        return key;
     }
 
     public String getFileName() {
         return fileName;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public void setKey(String key) {
+        this.key = key;
     }
 
     public void setFileName(String fileName) {
